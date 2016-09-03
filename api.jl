7723cbdb314fefc689c679d55cce1c52baa3883f
AnkitAggarwalPEC <ankit.ankit.aggarwal@gmail.com>
2016-09-03 23:11:02 +0530

    Added metadata support

--- api.jl
+++ api.jl
@@ -3,26 +3,66 @@ import Requests
 using Requests.get
 using Requests.Response
 
-metaDictionary = Dict{UTF8String,Any}()
+"""
+global definition of dictionary to store metadata per database
+"""
+metaDictionary = Dict{UTF8String,Dict{UTF8String,Any}}()
 
-function getQueryArgs(database_code:: AbstractString , per_page::Int = 100 ,
+
+"""
+function to return the GET arguements for downloading the meta data for
+the datasets in the particular database defined by database_code
+"""
+function getqueryargs(database_code:: AbstractString , per_page::Int = 100 ,
  sort_by="id" , page::Int = 1 )
 
     queryArgs = Dict{Any , Any}("database_code" => database_code,
                                 "per_page" => per_page,
                                 "sort_by" => sort_by,
                                 "page" =>page,
-                                "api_key" =>getApiKey())
+                                "api_key" =>getapikey())
 
     return queryArgs
 end
 
-function insertMetaData()
+"""
+function to insert metadata per database
+"""
+function insertmetadata(database_code::UTF8String , data :: Dict{UTF8String,Any})
+    metaDictionary[database_code] = data
 
+    #To check if insertion is successful
+    temp = get(metaDictionary , database_code , 0)
+
+    if temp == 0
+        error("Error in insertion")
+        return false
+    else
+        return true
+
+    end
 end
-    
+
+"""
+function to get the metadata from the metadata database
+"""
+function getmetadata(code::UTF8String) 
+
+   temp = get(metaDictionary , database_code , 0)
+
+    if temp == 0
+        error("Value doesn't exist in database")
+        return false
+    else
+        return temp
+
+    end 
 end
-function getApiKey()
+
+"""
+function to get the Quandl API key
+"""
+function getapikey()
     if !ispath(joinpath(pwd(),"token/"))
         error("Api Key is not initialized")
     end
@@ -39,37 +79,63 @@ function getApiKey()
 
 end
 
-function getBaseUrl()
+"""
+function to get the basic url for the Quandl
+"""
+function getbaseurl()
     path = "https://www.quandl.com/api/"
     return path
 end
 
-
-function getListOfDatasets(code::AbstractString)
+"""
+function to download the metadata for all the datasets present in the database ("NSE" , "WIKI")
+"""
+function getlistofdatasets(code::AbstractString)
 
     if length(code) == 0
         error("Please pass a valid code!")
     end
-    final_path = getBaseUrl() * "v3/datasets.json/"
-    println(final_path)
-
-    resp = get(final_path , query = getQueryArgs(code))
-
-    abc = Requests.json(resp)
-
-    println(abc)
-
-    #data = split(Requests.text(resp) , "\n")
-
-    #data = JSON.parse(resp.data)
+    final_path = getbaseurl() * "v3/datasets.json/"
+    
+    resp = get(final_path , query = getqueryargs(code))
 
     if resp.status != 200
         println("Error in processing the request")
     end 
 
+    data = Requests.json(resp)
+
+    insertmetadata(code , data["meta"])
+
+    storealldatasetscode(code)
+    return nothing
 end
 
-function set_auth_token(token::AbstractString)
+"""
+funciton to extract all the datasets code from metadata
+"""
+function storealldatasetscode(code::AbstractString)
+
+    if length(code) == 0
+        error("Please pass a valid code!")
+    end
+    metaDict = getmetadata(code)
+
+    if metaDict == false
+        error(code * "data doesn't exist in database !!!")
+    else 
+        total_pages = metaDict["total_pages"]
+        for i = 1:total_pages
+
+        end
+end 
+
+
+"""
+function to set the Quandl API key
+"""
+
+function setauthtoken(token::AbstractString)
 
     if length(token) != 20 && length(token) != 0
         error("Invalid Token : must be 20 characters long or be an empty")
@@ -92,6 +158,9 @@ function set_auth_token(token::AbstractString)
 end
 
 
+"""
+    Some random code
+"""
 println("Starting the quandl")
-set_auth_token("JHKaDwdS-RtM26RxPauV")
-getListOfDatasets("NSE")
+setauthtoken("JHKaDwdS-RtM26RxPauV")
+getlistofdatasets("NSE")
